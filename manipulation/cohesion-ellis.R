da904a7cb39fc2a422c1172dcfd8b4e7420e75da
Andriy V. Koval <koval.andrey@gmail.com>
2018-12-06 08:14:09 -0800

    stable graph of synthetic data

--- manipulation/cohesion-ellis.R
+++ manipulation/cohesion-ellis.R
@@ -20,7 +20,7 @@ cat("\f") # clear console when working in RStudio
 # ---- load-packages -----------------------------------------------------------
 # Attach these packages so their functions don't need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
 library(magrittr) #Pipes
-
+library(ggplot2)
 
 # ---- declare-globals ---------------------------------------------------------
 path_input <- "./data-public/raw/synthetic/synthetic.csv"
@@ -39,12 +39,41 @@ ds <- ds %>%
    ,rand = rank(value)
    ,top = ifelse(rank(value)==1,TRUE, FALSE)
   ) %>% 
-  dplyr::ungroup()
+  dplyr::ungroup() %>% 
+  # adjust the scales
+  dplyr::mutate(
+    region = as.factor(region)
+    ,interval = as.factor(interval)
+    ,channel = as.factor(channel)
+  )
 
 ds %>% dplyr::glimpse(50)
 
+# ---- basic-graph -----------------------
+
+d <- ds %>% 
+  dplyr::filter(top)
 
 
+g1 <- d %>% 
+  ggplot(aes(x = interval, y = channel, color = region))+
+  geom_line(aes(group=focal_channel))+
+  # geom_point(aes(size = mean), shape = 21)+
+  geom_point(shape = 20, size = 2)+
+  # scale_y_discrete(limits = c(1:22))+
+  coord_cartesian(ylim = c(1,22))+
+  geom_vline(xintercept = c(3.5, 5.5), linetype = "dashed")+
+  annotate(geom="text", x = 2, y = 21, label = "TASK 1")+
+  annotate(geom="text", x = 4.5, y = 21, label = "REST")+
+  annotate(geom="text", x = 7, y = 21, label = "TASK 2")+
+  labs(title = "Functional cohesion guided by the mode")+
+  theme_minimal()
+g1
+  
+g2 <- g1 +
+  facet_grid(region ~ . )
+g2
+  
 
 # ---- define-utility-functions ---------------
 
